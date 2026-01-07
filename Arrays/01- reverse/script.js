b740c3d9396534934a493e85ee54d9b0c37cad21
oscarvictiria <oscarvictoriagonzalez@gmail.com>
2026-01-06 20:46:37 -0500

    added folders. Completed replace function

---
+++ Arrays/01- reverse/script.js
@@ -0,0 +1,4 @@
+// Reverse an array in place
+// Example:
+// Input: [1, 2, 3, 4]
+// Output: [4, 3, 2, 1]
---
+++ Arrays/02-replace/script.js
@@ -0,0 +1,14 @@
+//  Replace a value at a given index
+// Example:
+// Input: ([10, 20, 30], index = 1, value = 99)
+// Output: [10, 99, 30]
+
+function replace(arr, index, value) {
+    arr[index] = value; 
+    return arr
+    
+}
+
+var arr =  [10, 99, 30]
+
+console.log(replace(arr, 0, 2));
